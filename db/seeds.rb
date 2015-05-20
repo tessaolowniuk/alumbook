78ad2050c8ca148ef0abecf1d5becd34b9455681
Tessa Olowniuk <tessaolowniuk@gmail.com>
2015-05-20 13:57:02 -0500

    Seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Logins.create(id: '1', username: 'user', password: 'password', first_name: 'John',
+  middle_initial: 'K', last_name: 'Stamos', type: '2')
+
+Logins.create(id: '2', username: 'user2', password: 'password', first_name: 'Conan',
+  middle_initial: 'L', last_name: 'OBrien', type: '1')
+
+Users.create(id: '1', login_id: '1', email_addr: 'stamos@hotmail.com',
+  street: '329 House St.', city: 'Chicago', state: 'IL', zip: '60540')
+
+Saved_Lists.create(id: '100', login_id: '100', list_name: 'First List')
+
+Saved_List_Users.create(id: '1', saved_list_id: '200', user_id: '1')
+
+Degrees.create(id: '300', major_name: 'Computer Science', type: '3')
+
+Colleges.create(id: '600', college_name: 'North Central College')
