d405f1a11e0941c092c91fb4b22b02422f2cf649
Victor Bashorun <vbashorun@gmail.com>
2015-06-09 12:14:09 -0500

    More survey relationships

--- app/models/survey_question.rb
+++ app/models/survey_question.rb
@@ -3,4 +3,5 @@ class SurveyQuestion < ActiveRecord::Base
   self.inheritance_column = nil
   belongs_to :survey
   has_many :survey_question_options
+  has_many :user_survey_responses
 end
--- app/models/user_survey.rb
+++ app/models/user_survey.rb
@@ -2,4 +2,5 @@
 class UserSurvey < ActiveRecord::Base
   belongs_to :User
   belongs_to :Survey
+  has_many :user_survey_responses
 end
