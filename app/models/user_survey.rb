b5fc714642e667ed3db38a49a9a2a72530a517fa
Jeff Mayer <thecomputerguy.jeff@gmail.com>
2015-06-10 07:33:45 -0500

    more enhancements in style of user survery

--- app/models/user_survey.rb
+++ app/models/user_survey.rb
@@ -1,5 +1,6 @@
 #Created by Tessa, Modified by Victor
 class UserSurvey < ActiveRecord::Base
+  self.inheritance_column = nil
   belongs_to :user
   belongs_to :survey
   has_many :user_survey_responses
